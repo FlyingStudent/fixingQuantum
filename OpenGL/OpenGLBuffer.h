ac34a495375d49e9f7add76adfc604e4846e015f
FlyingStudent <138330536+FlyingStudent@users.noreply.github.com>
2024-05-18 15:24:30 +0000

    opengl Quantum

---
+++ OpenGL/OpenGLBuffer.h
@@ -0,0 +1,41 @@
+#pragma once
+#include"Quantum/Renderer/Buffer.h"
+namespace Quantum {
+	class OpenGLVertexBuffer :public VertexBuffer {
+	public:
+		OpenGLVertexBuffer(uint32_t size);
+		OpenGLVertexBuffer(float* vertices, uint32_t size);
+		virtual ~OpenGLVertexBuffer();
+
+		virtual void Bind() const override;
+		virtual void UnBind() const override;
+
+		virtual void SetData( void* data, uint32_t size) override;
+
+		virtual const BufferLayout& getLayout() const override {
+			return m_Layout;
+		}
+		virtual void SetLayout(const BufferLayout& layout) override {
+			m_Layout = layout;
+		}
+
+	private:
+		uint32_t m_RendererID;
+		BufferLayout m_Layout;
+	};
+	class OpenGLIndexBuffer :public IndexBuffer {
+	public:
+		OpenGLIndexBuffer(uint32_t* indecies, uint32_t count);
+		virtual ~OpenGLIndexBuffer();
+
+		virtual void Bind() const;
+		virtual void UnBind() const;
+
+		virtual uint32_t GetCount() const {
+			return m_Count;
+		}
+	private:
+		uint32_t m_RendererID;
+		uint32_t m_Count;
+	};
+}
